dac421265df9577915d06f8ae408684d799f1ee4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-25 14:51:57 +0300

    axios operation and interfaces

---
+++ pages/api/addMessage.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { IMessage } from "./interfacesAndTypes";
+
+axios.defaults.baseURL =
+  "https://688337a621fa24876a9d2a34.mockapi.io/api/message/message";
+
+export const postMessage = async (messageData: IMessage) => {
+  console.log(messageData);
+  const data = await axios.post("", messageData);
+  return data.data;
+};
