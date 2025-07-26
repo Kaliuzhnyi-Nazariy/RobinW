31216c0b7287ad9aa93a9709a5761cdecc41caa2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-26 13:46:07 +0300

    add adaptive styles

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -4,45 +4,51 @@ import Section from "../helper/Section";
 const Experience = () => {
   return (
     <Section>
-      <p className=" text-[16px] text-[var(--lightGrey)] uppercase ">
+      <p className=" text-[16px] text-[var(--lightGrey)] uppercase font-bold ">
         work experience
       </p>
-      <h2 className="mt-2">Companies I have worked for in the past</h2>
-      <ul className="mt-10 flex flex-col gap-10">
-        <li>
+      <h2 className="mt-2 min-[768px]:w-[688px] min-[1440px]:w-[690px] ">
+        Companies I have worked for in the past
+      </h2>
+      <ul className="mt-10 flex flex-col gap-10 min-[768px]:flex-row min-[768px]:gap-6 ">
+        <li className="min-[768px]:w-[calc((100%-48px)/3)]">
           <span className="text-[117px] text-[var(--lightGrey)] opacity-50 ">
             01
           </span>
-          <h3>
+          <h3 className="min-[768px]:w-[197px] max-[1439px]:w-[197px] ">
             <span className="text-[var(--greenAccent)] ">Google</span>,
             Interaction Designer
           </h3>
-          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 ">
+          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 min-[768px]:w-[197px] min-[1440px]:w-[368px] ">
             I currently am the lead designer on the interaction design team for
             Google Play.
           </p>
         </li>
-        <li>
+        <li className="min-[768px]:w-[calc((100%-48px)/3)]">
           <span className="text-[117px] text-[var(--lightGrey)] opacity-50 ">
             02
           </span>
-          <h3>
-            <span className="text-blue-400 ">Facebook</span>, Product Designer
+          <h3 className="min-[768px]:w-[197px] max-[1439px]:w-[197px] ">
+            <span className="text-blue-400 ">Facebook</span>, Product{" "}
+            {/* <br className="hidden min-[768px]:block min-[1440px]:hidden " />{" "} */}
+            Designer
           </h3>
-          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 ">
+          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 min-[768px]:w-[197px] min-[1440px]:w-[368px] ">
             I’ve worked on a wide variety of internal tools for facebook over
             the past 6 years.
           </p>
         </li>
-        <li>
+        <li className="min-[768px]:w-[calc((100%-48px)/3)]">
           <span className="text-[117px] text-[var(--lightGrey)] opacity-50 ">
             03
           </span>
-          <h3>
-            <span className="text-[var(--pinkAccent)] ">Dribbble</span>, Graphic
-            Designer
+          <h3 className="min-[768px]:w-[197px] max-[1439px]:w-[197px] ">
+            <span className="text-[var(--pinkAccent)] ">Dribbble</span>
+            {/* <br className="hidden min-[768px]:block min-[1440px]:hidden " /> */}
+            , Graphic Designer
+            {/* <br className="hidden min-[768px]:block min-[1440px]:hidden " />{" "} */}
           </h3>
-          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 ">
+          <p className="text-[var(--lightGrey)] mt-4 text-[16px] opacity-50 min-[768px]:w-[197px] min-[1440px]:w-[368px] ">
             I started my design career with Dribbble. I was incharge of creating
             illustrations for the platform.
           </p>
