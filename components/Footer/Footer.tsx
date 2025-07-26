d3c9d8438de58b8e524dad95db46267a3cfc0ec6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-26 09:17:31 +0300

    added footer tag and remove unusable style

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,62 +4,68 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <Section extraStyles="bg-[var(--levelOne)] py-12 grid min-[768px]:grid-rows-[minmax(48px,_1fr)] min-[768px]:grid-cols-4 min-[768px]:pt-20 min-[768px]:pb-14 ">
-      <div className="min-[768px]:row-span-1">
-        <h1>Robin.W</h1>
-        <ul className="mt-8 flex gap-4">
-          <li>
-            <a href="https://x.com/" target="_blank">
-              <Image
-                src="/TwitterLogo.png"
-                alt="Twitter logo"
-                width={24}
-                height={24}
-              />
-            </a>
+    <footer>
+      <Section extraStyles="bg-[var(--levelOne)] grid min-[768px]:grid-rows-[minmax(48px,_1fr)] min-[768px]:grid-cols-4 min-[768px]:pt-20 min-[768px]:pb-14 ">
+        <div className="min-[768px]:row-span-1">
+          <h1>Robin.W</h1>
+          <ul className="mt-8 flex gap-4">
+            <li>
+              <a href="https://x.com/" target="_blank">
+                <Image
+                  src="/TwitterLogo.png"
+                  alt="Twitter logo"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://dribbble.com/" target="_blank">
+                <Image
+                  src="/DribbbleLogo.png"
+                  alt="Dribble logo"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/" target="_blank">
+                <Image
+                  src="/LinkedIn.png"
+                  alt="LinkedIn logo"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <ul className="mt-12 w-[154px] text-[16px] min-[768px]:col-3 min-[768px]:mt-0 min-[1440px]:justify-self-center">
+          <li className="w-full h-12 flex items-center cursor-pointer">Home</li>
+          <li className="w-full h-12 flex items-center cursor-pointer">
+            About
           </li>
-          <li>
-            <a href="https://dribbble.com/" target="_blank">
-              <Image
-                src="/DribbbleLogo.png"
-                alt="Dribble logo"
-                width={24}
-                height={24}
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/" target="_blank">
-              <Image
-                src="/LinkedIn.png"
-                alt="LinkedIn logo"
-                width={24}
-                height={24}
-              />
-            </a>
+          <li className="w-full h-12 flex items-center cursor-pointer">Work</li>
+          <li className="w-full h-12 flex items-center cursor-pointer">
+            Process
           </li>
         </ul>
-      </div>
-
-      <ul className="mt-12 w-[154px] text-[16px] min-[768px]:col-3 min-[768px]:mt-0 min-[1440px]:justify-self-center">
-        <li className="w-full h-12 flex items-center cursor-pointer">Home</li>
-        <li className="w-full h-12 flex items-center cursor-pointer">About</li>
-        <li className="w-full h-12 flex items-center cursor-pointer">Work</li>
-        <li className="w-full h-12 flex items-center cursor-pointer">
-          Process
-        </li>
-      </ul>
-      <ul className="w-[154px] text-[16px] ">
-        <li className="w-full h-12 flex items-center cursor-pointer">Store</li>
-        <li className="w-full h-12 flex items-center cursor-pointer">Blog</li>
-        <li className="w-full h-12 flex items-center cursor-pointer">
-          Reading List
-        </li>
-      </ul>
-      <p className="mt-12 text-[16px] text-[var(--lightGrey)] min-[768px]:col-end-3 min-[768px]:col-start-1 ">
-        Not Copyright 2020 • Robin Williams. Webflow cloneable
-      </p>
-    </Section>
+        <ul className="w-[154px] text-[16px] ">
+          <li className="w-full h-12 flex items-center cursor-pointer">
+            Store
+          </li>
+          <li className="w-full h-12 flex items-center cursor-pointer">Blog</li>
+          <li className="w-full h-12 flex items-center cursor-pointer">
+            Reading List
+          </li>
+        </ul>
+        <p className="mt-12 text-[16px] text-[var(--lightGrey)] min-[768px]:col-end-3 min-[768px]:col-start-1 ">
+          Not Copyright 2020 • Robin Williams. Webflow cloneable
+        </p>
+      </Section>
+    </footer>
   );
 };
 
