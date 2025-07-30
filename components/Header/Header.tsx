12266d6612d89a681f788c28fb9c755ac5906beb
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-30 14:39:30 +0300

    implement all changes in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavListItem from "./NavListItem";
 import Section from "../helper/Section";
+import Image from "next/image";
+import MenuBtn from "./MenuBtn";
 
 const Header = () => {
   return (
@@ -8,8 +10,8 @@ const Header = () => {
       extraStyles="w-full h-[72px] bg-transparent flex justify-between items-center min-md:h-[88px] "
       changedYPadding="p-0"
     >
-      <h1>Robin.W</h1>
-      <div className="w-6 h-6 bg-white hidden max-md:block"></div>
+      <Image src="Logo.svg" alt="Robin.W" width={79} height={24} />
+      <MenuBtn />
       <nav className="hidden min-md:block">
         <ul className="flex text-[16px] ">
           <NavListItem>Experience</NavListItem>
