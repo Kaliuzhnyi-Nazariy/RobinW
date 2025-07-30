ef69efa6f9b068f5905eccf88f3c6dc710963b54
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-07-30 14:40:54 +0300

    added mob logic + anchor links, and corrected styles

--- components/Header/NavListItem.tsx
+++ components/Header/NavListItem.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-const NavListItem = ({ children }: { children: React.ReactNode }) => {
+const NavListItem = ({
+  children,
+  closeMenu,
+}: {
+  children: React.ReactNode;
+  closeMenu?: () => void;
+}) => {
   return (
-    <li className="px-6 py-8 cursor-pointer hover:bg-[var(--primary)] transition-all hover:font-[700] ">
-      {children}
+    <li>
+      <a
+        href={`#${children}`}
+        className="min-[768px]:px-6 min-[768px]:py-8 min-[768px]:hover:bg-[var(--primary)] transition-all hover:font-[700] "
+        onClick={() => {
+          if (closeMenu) closeMenu();
+        }}
+      >
+        {children}
+      </a>
     </li>
   );
 };
